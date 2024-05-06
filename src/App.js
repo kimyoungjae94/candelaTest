0959ddd631ed71107e6f06cff6e4b6802ff81c84
kimyoungjae <osung2110.yk@gmail.com>
2024-05-06 18:31:13 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          aEdit <code>src/App.js</code> and save to reload.
+          aaaEdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className='App-link'
